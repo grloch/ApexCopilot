9af5a46a16cd388d14667fa3d14d2ca5bf6ed1f8
Gabriel Loch <grl.loch@gmail.com>
2024-04-17 20:46:37 -0300

    interfaces update

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -1,34 +1,33 @@
+type EnumNumberRange<N extends number, Acc extends number[] = []> = Acc['length'] extends N ? Acc[number] : EnumNumberRange<N, [...Acc, Acc['length']]>
+
 export namespace ProjectConfigOptions {
-  // TODO CONFIG LOGS AND USE OPTION!
   export type ProjectConfing = {
-    logs?: {
-      path: string
+    logs: {
+      path: string | undefined
       save?: boolean
     }
     manifest: {
-      mergedPath: string
-      path: string
+      mergedPath: string | undefined
+      path: string | undefined
     }
     package: {
       fullManifestPath: boolean
-      path: string
+      path: string | undefined
       timestamp: boolean
     }
   }
 }
 
 export namespace NumbersTypes {
-  type EnumNumberRange<N extends number, Acc extends number[] = []> = Acc['length'] extends N ? Acc[number] : EnumNumberRange<N, [...Acc, Acc['length']]>
-
   export type IntRange<F extends number, T extends number> = Exclude<EnumNumberRange<T>, EnumNumberRange<F>>
 }
 
 export namespace Logger {
-  type ErrorType = 'ERROR' | 'EXCEPTION'
+  export type ErrorType = 'ERROR' | 'EXCEPTION'
 
-  type LogType = 'INFO' | 'LOG' | 'METHOD_RETURN' | 'PROCESS_END' | 'PROCESS_START' | 'WARNING' | ErrorType
+  export type LogType = 'INFO' | 'LOG' | 'METHOD_RETURN' | 'PROCESS_END' | 'PROCESS_START' | 'WARNING' | ErrorType
 
-  type LogOptions = {
+  export interface LogOptions {
     context?: string
     message: string
     prompt?: boolean
