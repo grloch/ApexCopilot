f01d82bb500c78f0c6096b81930041c96eaba067
grloch <grl.loch@gmail.com>
2024-06-16 22:13:02 -0300

    created mergin package

--- src/helpers/prompt/index.ts
+++ src/helpers/prompt/index.ts
@@ -1,9 +1,11 @@
 import * as input from './input'
 import inquirePath from './inquirePath'
+import selectManifestFiles from './selectFiles'
 
 export default {
   input,
   path: {
     inquirePath,
+    selectManifestFiles,
   },
 }
