29a8c8ddeee4ec3678f10fc327fe05d8fab10185
grloch <grl.loch@gmail.com>
2024-06-23 17:27:19 -0300

    update stuffs

--- src/helpers/package.ts
+++ src/helpers/package.ts
@@ -1,102 +1,128 @@
-import * as Fs from 'fs-extra'
-import {json} from 'stream/consumers'
-import convert from 'xml-js'
+import * as Fs from 'fs';
+import convert from 'xml-js';
+
+import defaultLogger from './logger';
 
 type ManifestPackage = {
-  xml: {attributes: {version: '1.0'}}
-  Package: {
-    types: Array<ManifestType>
-  }
-}
+	Package: {
+		types: Array<ManifestType>;
+	};
+	xml: { attributes: { version: '1.0' } };
+};
+
+type XmlProprety = {
+	_text: string;
+};
 
 type ManifestType = {
-  members: Array<string>
-  name: string
-}
+	members: Array<XmlProprety | string>;
+	name: XmlProprety | string;
+};
 
 export class PackageController {
-  public file = {
-    xml: {
-      _attributes: {encoding: 'UTF-8', standalone: 'yes', version: '1.0'},
-    },
-    Package: {
-      types: [],
-      version: '58.0',
-    },
-  }
+	public file = {
+		Package: {
+			types: [],
+			version: '58.0',
+		},
+		xml: {
+			_attributes: {
+				encoding: 'UTF-8',
+				standalone: 'yes',
+				version: '1.0',
+			},
+		},
+	};
 
-  public members: Map<string, Set<string>> = new Map<string, Set<string>>()
+	public members: Map<string, Set<string>> = new Map<string, Set<string>>();
 
-  public addMemberItem(metadataName: string, metadataItem: string) {
-    if (!this.members.has(metadataName)) {
-      this.members.set(metadataName, new Set<string>())
-    }
+	get hasItens() {
+		return [...this.members.values()].flat().length > 0;
+	}
 
-    this.members.get(metadataName)?.add(metadataItem)
-  }
+	get membersNames() {
+		return this.members.keys();
+	}
 
-  public concatFile(filePath: string) {
-    let rawFile = {}
+	public addMemberItem(metadataName: string, metadataItem: string) {
+		if (!this.members.has(metadataName)) {
+			this.members.set(metadataName, new Set<string>());
+		}
 
-    try {
-      rawFile = JSON.parse(convert.xml2json(Fs.readFileSync(filePath, 'utf8'), {compact: true, spaces: 4}))
-    } catch (error) {
-      return
-    }
+		this.members.get(metadataName)?.add(metadataItem);
+	}
 
-    if (!rawFile.Package) rawFile.Package = {}
-    if (!rawFile.Package.types) rawFile.Package.types = []
+	public concatFile(filePaths: Array<string> | string) {
+		if (!filePaths) filePaths = [];
+		else if (!Array.isArray(filePaths)) filePaths = [filePaths];
 
-    if (!Array.isArray(rawFile.Package.types)) {
-      rawFile.Package.types = [rawFile.Package?.types]
-    }
+		for (const file of filePaths) {
+			if (!Fs.existsSync(file)) {
+				defaultLogger.error(`${file} isn't a valid path or not founded`, 'EXCEPTION');
+			}
 
-    for (const typeItem of rawFile.Package.types) {
-      if (!Array.isArray(typeItem.members) || !typeItem.members) typeItem.members = [typeItem.members]
+			let rawFile: ManifestPackage = {
+				Package: { types: [] },
+				xml: { attributes: { version: '1.0' } },
+			};
 
-      for (const memberItem of typeItem.members) {
-        this.addMemberItem(typeItem.name._text, memberItem._text)
-      }
-    }
-  }
+			try {
+				rawFile = JSON.parse(convert.xml2json(Fs.readFileSync(file, 'utf8'), { compact: true, spaces: 4 }));
+			} catch (error) {
+				return defaultLogger.error(`Error parsing file "${file}": ${error}`, 'EXCEPTION');
+			}
 
-  public parseToFile() {
-    const xmlFile: ManifestPackage = {
-      Package: {
-        types: [],
-      },
-    }
+			if (!Array.isArray(rawFile.Package.types)) {
+				rawFile.Package.types = [rawFile.Package?.types];
+			}
 
-    for (const metadataName of [...this.members.keys()].sort()) {
-      const pkgType = {
-        members: [],
-        name: metadataName,
-      }
+			for (const typeItem of rawFile.Package.types) {
+				if (!Array.isArray(typeItem.members) || !typeItem.members) typeItem.members = [typeItem.members];
 
-      let memberItens: Array<string> = [...this.members.get(metadataName)]
-      pkgType.members = memberItens.sort()
+				for (const memberItem of typeItem.members) {
+					this.addMemberItem(typeItem.name._text, memberItem._text);
+				}
+			}
+		}
+	}
 
-      this.file.Package.types.push(pkgType)
-    }
+	public parseToFile() {
+		const xmlFile: ManifestPackage = {
+			Package: {
+				types: [],
+			},
+		};
 
-    return convert.json2xml(this.file, {compact: true, spaces: 4})
-  }
+		for (const metadataName of [...this.members.keys()].sort()) {
+			const pkgType = {
+				members: [],
+				name: metadataName,
+			};
+
+			let memberItens: Array<string> = [...this.members.get(metadataName)];
+			pkgType.members = memberItens.sort();
+
+			this.file.Package.types.push(pkgType);
+		}
+
+		return convert.json2xml(this.file, { compact: true, spaces: 4 });
+	}
 }
 
 export function xml2json(filePath: string) {
-  const packageFile = new PackageController()
+	const packageFile = new PackageController();
 
-  const result = JSON.parse(convert.xml2json(Fs.readFileSync(filePath, 'utf8'), {compact: true, spaces: 4}))
+	const result = JSON.parse(convert.xml2json(Fs.readFileSync(filePath, 'utf8'), { compact: true, spaces: 4 }));
 
-  if (!Array.isArray(result.Package?.types)) result.Package.types = [result.Package?.types]
+	if (!Array.isArray(result.Package?.types)) result.Package.types = [result.Package?.types];
 
-  for (const typeItem of result.Package.types) {
-    if (!Array.isArray(typeItem.members)) typeItem.members = [typeItem.members]
+	for (const typeItem of result.Package.types) {
+		if (!Array.isArray(typeItem.members)) typeItem.members = [typeItem.members];
 
-    for (const memberItem of typeItem.members) {
-      packageFile.addMemberItem(typeItem.name._text, memberItem._text)
-    }
-  }
+		for (const memberItem of typeItem.members) {
+			packageFile.addMemberItem(typeItem.name._text, memberItem._text);
+		}
+	}
 
-  return packageFile
+	return packageFile;
 }
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,84 +1,34 @@
-// import * as Fs from 'fs-extra'
-// import * as convert from 'xml-js'
-// import { logger } from "./log/logger";
-
 export function prettyNum(num: number) {
-  return num < 10 ? `0${num}` : String(num)
+	return num < 10 ? `0${num}` : String(num);
 }
 
 export function getTimeStamp() {
-  const now = new Date()
+	const now = new Date();
 
-  const year = String(now.getFullYear())
-  const mounth = prettyNum(now.getMonth())
-  const day = prettyNum(now.getDate())
-  const hour = prettyNum(now.getHours())
-  const minute = prettyNum(now.getMinutes())
-  const second = prettyNum(now.getSeconds())
+	const year = String(now.getFullYear());
+	const mounth = prettyNum(now.getMonth());
+	const day = prettyNum(now.getDate());
+	const hour = prettyNum(now.getHours());
+	const minute = prettyNum(now.getMinutes());
+	const second = prettyNum(now.getSeconds());
 
-  return `${now.getTime()} ${year}-${mounth}-${day} ${hour}:${minute}:${second}`
+	return `${now.getTime()} ${year}-${mounth}-${day} ${hour}:${minute}:${second}`;
 }
-
-// export function deletePath(deletedPath: string) {
-//   if (Fs.existsSync(deletedPath)) {
-//     Fs.rmSync(deletedPath, { recursive: true });
-//     logger.methodResponse(`tools/utils.ts/deletePath (${deletedPath})`, true);
-//   } else {
-//     logger.methodResponse(`tools/utils.ts/deletePath (${deletedPath})`, false);
-//   }
-// }
-
-// export function movePath(oldPath: string, newPath: string) {
-//   Fs.moveSync(oldPath, newPath);
-//   logger.methodResponse("tools/utils.ts/movePath", `${oldPath} => ${newPath}`);
-// }
-
-// export function copyFile(oldPath: string, newPath: string) {
-//   Fs.copyFileSync(oldPath, newPath);
-//   logger.methodResponse("tools/utils.ts/copyFile", `${oldPath} => ${newPath}`);
-// }
-
-// export function createDir(dirPath: string) {
-//   if (!Fs.existsSync(dirPath)) {
-//     Fs.mkdirSync(dirPath, { recursive: true });
-
-//     logger.methodResponse(`tools/utils.ts/createDir (${dirPath})`, dirPath);
-//   } else {
-//     logger.methodResponse(`tools/utils.ts/createDir (${dirPath})`, false);
-//   }
-// }
-
-// export function xml2json(filePath: string) {
-//   logger.log({ message: "Parsing " + filePath });
-
-//   try {
-//     let rawFile = Fs.readFileSync(filePath).toString();
-
-//     if (rawFile == "") return null;
-
-//     return JSON.parse(convert.xml2json(rawFile, {}));
-//   } catch (error) {
-//     logger.error({ message: `Fail while parsing ${filePath}:` });
-//     logger.error({ message: error + "" });
-
-//     return null;
-//   }
-// }
 
 export function capitalize(txt: string) {
-  return txt.charAt(0).toUpperCase() + txt.slice(1)
+	return txt.charAt(0).toUpperCase() + txt.slice(1);
 }
 
-//  function loadConfig() {
-//   // import defaultConfig from './defaultConfig.json';
+export function mergeJson(target: any, model: any): void {
+	for (const key in model) {
+		if (model[key]) {
+			if (typeof model[key] === 'object' && model[key] !== null) {
+				if (!target[key]) target[key] = {};
 
-//   // global projeto
-//   // projeto
-//   // global
-
-//   return {
-//     logs: {path: './logs/apex-copilot', save: false},
-//   }
-// }
-
-
+				mergeJson(target[key], model[key]);
+			} else if (target[key] === undefined || target[key] === null) {
+				target[key] = model[key];
+			}
+		}
+	}
+}
