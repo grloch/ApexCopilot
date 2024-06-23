9e7a393ab501c52850f13c3deef22df5c49072b6
grloch <grl.loch@gmail.com>
2024-06-23 16:42:49 -0300

    update interfaces.d.ts

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -1,37 +1,37 @@
-type EnumNumberRange<N extends number, Acc extends number[] = []> = Acc['length'] extends N ? Acc[number] : EnumNumberRange<N, [...Acc, Acc['length']]>
+type EnumNumberRange<N extends number, Acc extends number[] = []> = Acc['length'] extends N ? Acc[number] : EnumNumberRange<N, [...Acc, Acc['length']]>;
 
 export namespace ProjectConfigOptions {
-  export type ProjectConfing = {
-    logs: {
-      path: string | undefined
-      save?: boolean
-    }
-    manifest: {
-      mergedPath: string | undefined
-      path: string | undefined
-    }
-    package: {
-      fullManifestPath: boolean
-      path: string | undefined
-      timestamp: boolean
-    }
-  }
+	export type ProjectConfing = {
+		logs: {
+			path: string;
+			save?: boolean;
+		};
+		manifest: {
+			mergedPath: string;
+			path: string;
+		};
+		package: {
+			fullManifestPath: boolean;
+			path: string;
+			timestamp: boolean;
+		};
+	};
 }
 
 export namespace NumbersTypes {
-  export type IntRange<F extends number, T extends number> = Exclude<EnumNumberRange<T>, EnumNumberRange<F>>
+	export type IntRange<F extends number, T extends number> = Exclude<EnumNumberRange<T>, EnumNumberRange<F>>;
 }
 
 export namespace Logger {
-  export type ErrorType = 'ERROR' | 'EXCEPTION'
+	export type ErrorType = 'ERROR' | 'EXCEPTION';
 
-  export type LogType = 'INFO' | 'LOG' | 'METHOD_RETURN' | 'PROCESS_END' | 'PROCESS_START' | 'WARNING' | ErrorType
+	export type LogType = 'INFO' | 'LOG' | 'METHOD_RETURN' | 'PROCESS_END' | 'PROCESS_START' | 'WARNING' | ErrorType;
 
-  export interface LogOptions {
-    context?: string
-    message: string
-    prompt?: boolean
-    throwError?: boolean
-    type?: LogType
-  }
+	export interface LogOptions {
+		context?: string;
+		message: string;
+		prompt?: boolean;
+		throwError?: boolean;
+		type?: LogType;
+	}
 }
