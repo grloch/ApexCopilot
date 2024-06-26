d731935380fde8a870919d237f2cb0cb829292ee
Gabriel <grl.loch@gmail.com>
2024-06-26 20:18:48 -0300

    changed json printing

--- src/helpers/print.ts
+++ src/helpers/print.ts
@@ -6,29 +6,48 @@ export function printTitle(text: string) {
 	console.log('\n' + color.black(color.white_b(`${message}`)));
 }
 
-export function printJson(jsonVariable: any, spacer: string = '') {
-	const keys = Object.keys(jsonVariable).sort();
+export function printJson(jsonVariable: any, deep: number = 0) {
+	let response = '';
+	const spacer = ' '.repeat(deep);
 
-	for (const key of keys) {
-		if (typeof jsonVariable[key] === 'object') {
-			console.log(`|${spacer} ${color.bold(key)}`);
-			printJson(jsonVariable[key], spacer + '..|');
-		} else {
-			let dataValue = jsonVariable[key];
+	if (jsonVariable !== undefined && jsonVariable !== null) {
+		const keys = Object.keys(jsonVariable).sort();
 
-			switch (typeof dataValue) {
-				case 'boolean': {
-					dataValue = color.red(String(dataValue));
-					break;
+		const objectKeys = keys.filter((i) => typeof jsonVariable[i] === 'object');
+		const variableKeys = keys.filter((i) => typeof jsonVariable[i] !== 'object');
+
+		for (const key of [...variableKeys, ...objectKeys]) {
+			if (typeof jsonVariable[key] === 'object') {
+				response += `├─ ${spacer}${color.bold(key)}: `;
+
+				if (jsonVariable[key] === undefined || jsonVariable[key] === null) response += jsonVariable[key];
+
+				response += '\n';
+
+				response += printJson(jsonVariable[key], deep + 1);
+			} else {
+				let dataValue = jsonVariable[key];
+
+				switch (typeof dataValue) {
+					case 'boolean': {
+						dataValue = color.red(String(dataValue));
+						break;
+					}
+
+					case 'string': {
+						dataValue = color.underline(color.yellow(`"${String(dataValue)}"`));
+						break;
+					}
+
+					default: {
+						dataValue = String(dataValue);
+					}
 				}
 
-				case 'string': {
-					dataValue = color.underline(color.yellow(String(dataValue)));
-					break;
-				}
+				response += `${deep === 0 ? '├─' : '│'}${spacer} ${deep === 0 ? '' : '├─'}${key}: ${dataValue}\n`;
 			}
-
-			console.log(`|${spacer}  . ${key}  ${dataValue}`);
 		}
 	}
+
+	return response;
 }
