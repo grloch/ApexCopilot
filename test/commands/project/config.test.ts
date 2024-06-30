3f5a8b08a9ec2db5fa9cb10001c774b7341131c9
Gabriel <grl.loch@gmail.com>
2024-06-30 12:42:00 -0300

    removed files

---
+++ test/commands/project/config.test.ts
@@ -0,0 +1,14 @@
+import { runCommand } from '@oclif/test';
+import { expect } from 'chai';
+
+describe('project:org', () => {
+	it('runs project:org cmd', async () => {
+		const { stdout } = await runCommand('project:config');
+		expect(stdout).to.contain('hello world');
+	});
+
+	it('runs project:org --name oclif', async () => {
+		const { stdout } = await runCommand('project:org --name oclif');
+		expect(stdout).to.contain('hello oclif');
+	});
+});
