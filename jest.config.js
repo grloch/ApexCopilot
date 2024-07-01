a8c3e0430d52644f95088d7f4838b88bd506f8fd
Gabriel <grl.loch@gmail.com>
2024-07-01 19:10:29 -0300

    jest update

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /* eslint-disable unicorn/prefer-module */
 
 module.exports = {
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	coverageReporters: ['json', 'lcov', 'text', 'clover'],
 	moduleFileExtensions: ['ts', 'js', 'json', 'node'],
 	preset: 'ts-jest',
 	testEnvironment: 'node',
